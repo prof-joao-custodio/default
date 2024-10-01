a6a1397aeb370fee357f6ff7a89900a291d3a925
prof-joao-custodio <joao.victor@profjoaocustodio.com.br>
2024-09-30 23:56:00 -0300

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -1,12 +1,20 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+// components/navigation/navbar.jsx
+
 import styles from "../../styles/Navbar.module.css";
+
 export default function Navbar() {
-	return (
-		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
-			</a>
-			<ConnectButton></ConnectButton>
-		</nav>
-	);
+  return (
+    <nav className={styles.navbar}>
+      {/* Professor João Custódio - 70999 */}
+      <div className={styles.candidateInfo}>
+        <span className={styles.candidateName}>Professor João Custódio</span>
+        <span className={styles.candidateNumber}>70999</span>
+      </div>
+
+      {/* Campaign Slogan */}
+      <div className={styles.slogan}>
+        Paz, Amor e Tradição por + Educação e Cultura para Cubatão
+      </div>
+    </nav>
+  );
 }
