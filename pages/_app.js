3cd76ea40b14108629e1e66e8b1ad774b5cda287
prof-joao-custodio <joao.victor@profjoaocustodio.com.br>
2024-10-01 00:39:22 -0300

    123
    456

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,80 @@
 import "../styles/globals.css";
 import MainLayout from "../layout/mainLayout";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag && window.gtag('config', 'G-YLN38YDZLR', {
+        page_path: url,
+      });
+      window.fbq && window.fbq('track', 'PageView');
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Head>
         <title>Professor João Custódio - Campanha 2024</title>
-        <meta name="description" content="Eleja Professor João Custódio - Paz, Amor e Tradição. Campanha Eleição 2024." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="Professor João Custódio, eleição 2024, vereador, campanha, paz, amor, tradição" />
-        <meta property="og:title" content="Professor João Custódio - Campanha 2024" />
-        <meta property="og:description" content="Paz, Amor e Tradição. Conheça a campanha de Professor João Custódio." />
-        <meta property="og:image" content="https://i.ibb.co/rx4VzcP/455686013-1038474321228600-8459096745685041423-n.jpg" />
-        <meta property="og:url" content="https://www.instagram.com/profjoaocustodio/" />
-        <meta property="og:type" content="website" />
-        <meta name="robots" content="index, follow" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Vote no Professor João Custódio para vereador - Campanha Eleição 2024 - Paz, Amor e Tradição." />
+
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NHDVMH3L');
+          `,
+        }} />
+
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1564396054163031');
+            fbq('track', 'PageView');
+          `,
+        }} />
       </Head>
+
+      {/* Noscript for Google Tag Manager */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-NHDVMH3L"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
+      {/* Noscript for Meta Pixel */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=1564396054163031&ev=PageView&noscript=1"
+        />
+      </noscript>
+
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
