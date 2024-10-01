47d4d997f5da51a7e4f5673dcf341c4f39b80869
prof-joao-custodio <joao.victor@profjoaocustodio.com.br>
2024-10-01 00:24:54 -0300

    1
    2

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       {/* Professor João Custódio - 70999 */}
       <div className={styles.candidateInfo}>
-        <span className={styles.candidateName}>Professor João Custódio</span>
-        <span className={styles.candidateNumber}>70999</span>
+        <span className={styles.candidateName}>Siga-nos em nossas redes sociais</span>
+        <span className={styles.candidateNumber}>Acompanhe o nosso trabalho</span>
       </div>
 
       {/* Campaign Slogan */}
       <div className={styles.slogan}>
-        Paz, Amor e Tradição por + Educação e Cultura para Cubatão
+        Clique nos botões abaixo.
       </div>
     </nav>
   );
