2be2f913dd4d4d8bb278bb49576c38c8a5ba918c
prof-joao-custodio <joao.victor@profjoaocustodio.com.br>
2024-10-01 09:13:42 -0300

    CNPJ novamente
    CNPJ novamente no header.

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -7,13 +7,13 @@ export default function Navbar() {
     <nav className={styles.navbar}>
       {/* Professor João Custódio - 70999 */}
       <div className={styles.candidateInfo}>
-        <span className={styles.candidateName}>Siga-nos em nossas redes sociais</span>
+        <span className={styles.candidateName}>Candidato a Vereador em Cubatão - Prof. João Custódio</span>
         <span className={styles.candidateNumber}>Acompanhe o nosso trabalho</span>
       </div>
 
       {/* Campaign Slogan */}
       <div className={styles.slogan}>
-        Clique nos botões abaixo.
+      ELEIÇÃO 2024 JOÃO VICTOR DAMASCENO RODRIGUES VEREADOR CNPJ 56.164.328/0001-06
       </div>
     </nav>
   );
